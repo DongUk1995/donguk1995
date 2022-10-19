466c6f8e0e344238f837df195309a6bee190307a
DongUk1995 <frlaehdd@naver.com>
2022-10-19 13:17:35 +0900

    13:17

--- air_bnb_backend/houses/admin.py
+++ air_bnb_backend/houses/admin.py
@@ -6,11 +6,11 @@ from .models import House
 class HouseAdmin(admin.ModelAdmin):
     fields = (
         "name",
-        "address", 
+        "address",
         ("price_per_night", "pets_allowed"),
     )
-    list_display =("name", "price_per_night", "address", "pets_allowed") 
-    list_filter = ("price_per_night","pets_allowed")
+    list_display = ("name", "price_per_night", "address", "pets_allowed")
+    list_filter = ("price_per_night", "pets_allowed")
     search_fields = ("address",)
     list_display_links = ("name", "address")
     list_editable = ("pets_allowed",)
--- air_bnb_backend/houses/models.py
+++ air_bnb_backend/houses/models.py
@@ -1,20 +1,23 @@
 from tabnanny import verbose
+import django
 from django.db import models
 
+
 class House(models.Model):
-    
+
     """model Definition for House"""
 
     name = models.CharField(max_length=140)
     price_per_night = models.PositiveBigIntegerField(
-        verbose_name = "Price", help_text ="Positive Numbers Only" 
-        )
+        verbose_name="Price", help_text="Positive Numbers Only"
+    )
     description = models.TextField()
     address = models.CharField(max_length=140)
     pets_allowed = models.BooleanField(
-        verbose_name = "Pets Allowed?",
+        verbose_name="Pets Allowed?",
         default=True,
-        help_text = "DOes this house allow pets?")
+        help_text="DOes this house allow pets?",
+    )
 
     def __str__(self):
         return self.name  ##str 메서드를 설정하는 것
