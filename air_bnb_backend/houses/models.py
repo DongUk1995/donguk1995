619f621782b15ead28d83331d163cb595811131b
DongUk1995 <frlaehdd@naver.com>
2022-10-19 12:22:56 +0900

    2022.10.19

--- air_bnb_backend/houses/models.py
+++ air_bnb_backend/houses/models.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from django.db import models
 
 class House(models.Model):
@@ -5,7 +6,15 @@ class House(models.Model):
     """model Definition for House"""
 
     name = models.CharField(max_length=140)
-    price_per_night = models.PositiveBigIntegerField()
+    price_per_night = models.PositiveBigIntegerField(
+        verbose_name = "Price", help_text ="Positive Numbers Only" 
+        )
     description = models.TextField()
     address = models.CharField(max_length=140)
-    pets_allowed = models.BooleanField(default=True)
+    pets_allowed = models.BooleanField(
+        verbose_name = "Pets Allowed?",
+        default=True,
+        help_text = "DOes this house allow pets?")
+
+    def __str__(self):
+        return self.name  ##str 메서드를 설정하는 것
