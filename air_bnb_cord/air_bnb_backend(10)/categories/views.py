cf8069b4d6270be375ac4e19d8d31d0ea6e7d768
DongUk1995 <frlaehdd@naver.com>
2022-11-03 15:40:04 +0900

    2022.11.03

--- air_bnb_cord/air_bnb_backend(10)/categories/views.py
+++ air_bnb_cord/air_bnb_backend(10)/categories/views.py
@@ -4,13 +4,19 @@ from .models import Category
 from .serializers import CategorySerializer  # 번역기를 만들었다.
 
 
+@api_view(["GET", "POST"])
+def categories(request):
+    if request.method == "GET":
+        all_categories = Category.objects.all()
+        serializer = CategorySerializer(all_categories, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        print(request.data)
+        return Response({"created": True})
+
+
 @api_view()
-def categories(requeset):
-    all_categories = Category.objects.all()
-    serializer = CategorySerializer(all_categories, many=True)
-    return Response(
-        {
-            "ok": True,
-            "categories": serializer.data,
-        }
-    )
+def category(request, pk):  ## url로 부터 호출된 모든 view들은 request 객체를 받는다.
+    category = Category.objects.get(pk=pk)
+    serializer = CategorySerializer(category)
+    return Response(serializer.data)
