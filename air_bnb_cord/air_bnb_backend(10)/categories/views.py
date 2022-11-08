a4c6c7a02a524ffe75eeb516c15168883320a895
DongUk1995 <frlaehdd@naver.com>
2022-11-08 19:32:21 +0900

    2022.11.08

--- air_bnb_cord/air_bnb_backend(10)/categories/views.py
+++ air_bnb_cord/air_bnb_backend(10)/categories/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from .models import Category
 from .serializers import CategorySerializer  # 번역기를 만들었다.
@@ -21,8 +22,24 @@ def categories(request):
             return Response(serializer.errors)
 
 
-@api_view()
-def category(request, pk):  ## url로 부터 호출된 모든 view들은 request 객체를 받는다.
-    category = Category.objects.get(pk=pk)
-    serializer = CategorySerializer(category)
-    return Response(serializer.data)
+@api_view(["GET", "PUT"])
+def category(request, pk):
+    try:
+        category = Category.objects.get(pk=pk)
+    except Category.DoesNotExist:
+        raise NotFound
+
+    if request.method == "GET":
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = CategorySerializer(
+            category,
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            updated_category = serializer.save()
+            return Response(CategorySerializer(updated_category).data)
+        else:
+            return Response(serializer.errors)
