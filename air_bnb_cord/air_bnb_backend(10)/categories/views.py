8fd20bd0811fe17f5a342abd206a50d184aa4ff5
DongUk1995 <frlaehdd@naver.com>
2022-11-16 20:49:33 +0900

    2022.11.16

--- air_bnb_cord/air_bnb_backend(10)/categories/views.py
+++ air_bnb_cord/air_bnb_backend(10)/categories/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
+from rest_framework.status import HTTP_404_NOT_FOUND
 from .models import Category
 from .serializers import CategorySerializer  # 번역기를 만들었다.
 
@@ -22,7 +23,7 @@ def categories(request):
             return Response(serializer.errors)
 
 
-@api_view(["GET", "PUT"])
+@api_view(["GET", "PUT", "DELETE"])
 def category(request, pk):
     try:
         category = Category.objects.get(pk=pk)
@@ -43,3 +44,9 @@ def category(request, pk):
             return Response(CategorySerializer(updated_category).data)
         else:
             return Response(serializer.errors)
+
+    elif request.method == "DELETE":
+        category.delete()
+        return Response(
+            status=HTTP_404_NOT_FOUND,
+        )
