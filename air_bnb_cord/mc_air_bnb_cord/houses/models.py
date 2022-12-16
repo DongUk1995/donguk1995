b21443df6fcac4b7c1121171e5517404cc76b3ab
김동욱 <105752138+DongUk1995@users.noreply.github.com>
2022-12-16 21:25:40 +0900

    ㄹㅇㄹ

--- air_bnb_cord/mc_air_bnb_cord/houses/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class House(models.Model):
-
-    name = models.CharField(max_length=140)
-    price = models.PositiveIntegerField()
-    description = models.TextField()
-    address = models.CharField(max_length=140)
