d968fc9a52520a7c26b8f825f80e69341103e5d1
김동욱 <email@example.com>
2022-12-28 22:08:41 +0900

    2022.12.28

--- air_bnb_cord/air_bnb_project/config/urls.py
+++ air_bnb_cord/air_bnb_project/config/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/vi/tweets/", include("tweets.urls")),
+    path("api/vi/users/", include("users.urls")),
 ]
